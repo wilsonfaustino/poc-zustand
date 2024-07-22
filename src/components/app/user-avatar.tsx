0bb99b23b623ebff989c8e8be6bd1115cf842b08
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-07-22 07:55:02 -0300

    refactor: Update UserAvatar component to include fallback for missing avatar image

--- src/components/app/user-avatar.tsx
+++ src/components/app/user-avatar.tsx
@@ -2,7 +2,7 @@ import { useShallow } from 'zustand/react/shallow'
 
 import { useStore } from '@/app/store'
 
-import { Avatar, AvatarImage } from '../ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 export const UserAvatar = () => {
   const { user } = useStore(
@@ -14,6 +14,7 @@ export const UserAvatar = () => {
   return (
     <Avatar className="size-40">
       <AvatarImage src={`https://github.com/${user}.png`} alt={`Avatar @${user}`} />
+      <AvatarFallback className="text-5xl">404</AvatarFallback>
     </Avatar>
   )
 }
