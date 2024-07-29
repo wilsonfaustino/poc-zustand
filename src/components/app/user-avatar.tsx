52a557641c8cf84fb1f7cf6d37cd4f0d601e993f
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-07-29 12:29:30 -0300

    refactor: Update UserAvatar component to use useUser hook for accessing user data

--- src/components/app/user-avatar.tsx
+++ src/components/app/user-avatar.tsx
@@ -1,19 +1,13 @@
-import { useShallow } from 'zustand/react/shallow'
-
-import { useStore } from '@/app/store'
+import { useUser } from '@/app/hooks/use-user'
 
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 export const UserAvatar = () => {
-  const { user } = useStore(
-    useShallow((state) => ({
-      user: state.user.data.userName,
-    }))
-  )
+  const { userName } = useUser()
 
   return (
     <Avatar className="size-40">
-      <AvatarImage src={`https://github.com/${user}.png`} alt={`Avatar @${user}`} />
+      <AvatarImage src={`https://github.com/${userName}.png`} alt={`Avatar @${userName}`} />
       <AvatarFallback className="text-5xl">404</AvatarFallback>
     </Avatar>
   )
